ac5ad4688820661879e60935296202ba2d440b60
rifat-sarker <rifatswd@gmail.com>
2025-04-06 22:09:01 +0600

    Upsert Operations and Select Queries

--- src/find.ts
+++ src/find.ts
@@ -9,18 +9,33 @@ const main = async () => {
   // find first and find first or throw error -- find first post that matches the condition - just like find in js
   const firstPost = await prisma.post.findFirstOrThrow({
     where: {
-      title: "Tittle",
+      title: "Title6",
     },
+    select: {
+      title : true,
+      content: true
+    }
   });
   console.log({ firstPost });
 
   // find unique and find uniqueOrThrow -- have to use unique field
   const uniquePost = await prisma.post.findUnique({
     where: {
-      id: 1,
+      id: 92,
     },
+    select: {
+      content: true,
+    }
   });
   console.log({ uniquePost });
 };
 
 main();
+
+/*
+*select: to select specific fields.
+* we can use select for create,update,delete,find operations
+
+
+
+*/
--- src/update.ts
+++ src/update.ts
@@ -3,30 +3,45 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const updates = async () => {
-  const singleUpdate = await prisma.post.update({
+  //   const singleUpdate = await prisma.post.update({
+  //     where: {
+  //       id: 81,
+  //     },
+  //     data: {
+  //       title: "Title5",
+  //     },
+  //   });
+  // //   console.log(singleUpdate);
+  //   const updateMany = await prisma.post.updateMany({
+  //     where: {
+  //       title: "Title5",
+  //     },
+  //     data: {
+  //       published: true,
+  //     },
+  //   });
+  //   console.log(updateMany);
+
+  const upsertData = await prisma.post.upsert({
     where: {
-      id: 81,
+      id: 92,
     },
-    data: {
-      title: "Title5",
+    update: {
+      title: "Title6",
+    },
+    create: {
+      title: "Title7",
+      content: "content7",
     },
   });
-//   console.log(singleUpdate);
-  const updateMany = await prisma.post.updateMany({
-    where: {
-      title: "Title5",
-    },
-    data: {
-      published: true,
-    },
-  });
-  console.log(updateMany);
+  console.log(upsertData);
 };
 updates();
 
 /** NOTES
  update vs updateMany
- update: single update , directly shows updated object
- updateMany: multiple update, shows count of updated recoards
+ * update: single update , directly shows updated object
+ * updateMany: multiple update, shows count of updated recoards
  by default updateAt takes same time as current time but when we update recoard updatedAt update automatically.
+ * upsert: if recoard is present it will update otherwise it will create new recoard
  */
