f3ff949221dd3d7a2e125c8edf9d7c58d5a82774
rifat-sarker <rifatswd@gmail.com>
2025-04-10 07:53:59 +0600

    Implementing Relation Filters

--- src/relational-queries.ts
+++ src/relational-queries.ts
@@ -2,36 +2,44 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-
 const relationalQueries = async () => {
+  //fluent api
+  // const result = await prisma.user.findUnique({
+  //     where: {
+  //         id: 1
+  //     }
+  // }).post();
 
-    //fluent api
-    // const result = await prisma.user.findUnique({
-    //     where: {
-    //         id: 1
-    //     }
-    // }).Post();
+  //
 
-    //
+  // include query -  show both user and post info - like populate in mongoose
+  const result = await prisma.user.findUnique({
+    where: {
+      id: 1,
+    },
+    include: {
+      post: true,
+    },
+  });
+  // console.log(result);
 
-    // include query -  show both user and post info - like populate in mongoose
-    const result = await prisma.user.findUnique({
-        where: {
-            id: 1
-        },
-        include: {
-            Post: true
+  const publishedPostUsers = await prisma.user.findMany({
+    include: {
+        post: {
+            where: {
+               published: true
+            }
         }
-    })
-    console.log(result);
-}
+    }
+  });
+  console.dir(publishedPostUsers, {depth: Infinity});
+};
 
 relationalQueries();
 
-
 /** notes
- * Fluent api -- use one to get another one 
+ * Fluent api -- use one to get another one
  * include query -- just like populate in mongoose
- * 
- * 
- * */ 
+ *
+ *
+ * */
